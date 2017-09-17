066e9986d3b7c50858e8746330c797ef9528b3dd
Hemanth <Hemanth@HEMANTH-PC>
2017-09-17 17:48:10 +0530

    Add project files.

---
+++ AngularJsValidations/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularJsValidations.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AngularJsValidations/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AngularJsValidations.Startup))]
+namespace AngularJsValidations
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
